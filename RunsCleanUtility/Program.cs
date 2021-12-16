812c690a99c1dc4d638d1fef16a0eb07d297489b
KirillAldashkin <a89086520092k@yandex.ru>
2021-12-16 22:07:05 +0800

    Fix deleting workflow runs

--- RunsCleanUtility/Program.cs
+++ RunsCleanUtility/Program.cs
@@ -1,8 +1,8 @@
 ﻿var repo = args.Length > 0 ? args[0] : Console.ReadLine();
 var token = args.Length > 1 ? args[1] : Console.ReadLine();
 
-string runsUrl(int page) => $"https://api.github.com/repos/{repo}/actions/runs?per_page=100&page={page}";
-string deleteUrl(int id) => $"https://api.github.com/repos/{repo}/actions/runs/{id}/logs";
+var runsUrl = $"https://api.github.com/repos/{repo}/actions/runs?per_page=100";
+string deleteUrl(int id) => $"https://api.github.com/repos/{repo}/actions/runs/{id}";
 
 HttpClient client = new();
 client.DefaultRequestHeaders.Add("Authorization", $"token {token}");
@@ -11,20 +11,27 @@ client.DefaultRequestHeaders.Add("User-Agent", "Actions cleanup utility");
 static async Task<T> Get<T>(HttpClient client, string url) =>
     System.Text.Json.JsonSerializer.Deserialize<T>(await client.GetStringAsync(url));
 
-int page = 0;
 while (true)
 {
-    var workflows = await Get<Workflows>(client, runsUrl(++page));
-    if (workflows.workflow_runs.Length == 0) break;
-    Console.WriteLine($">>>> [Page {page}] {workflows.total_count} workflow runs remains");
+    bool any = false;
+    var workflows = await Get<Workflows>(client, runsUrl);
+    Console.WriteLine($">>>> {workflows.total_count} workflow runs remains");
     foreach(var run in workflows.workflow_runs)
     {
-        Console.WriteLine($"<< Deleting: {run}");
-        var response = await client.DeleteAsync(deleteUrl(run.id));
-        var content = await response.Content.ReadAsStringAsync();
-        Console.WriteLine($">> Code {(int)response.StatusCode}: {content}");
+        if (run.status != "completed")
+            Console.WriteLine($"<< Skipping: {run}");
+        else
+        {
+            any = true;
+            Console.WriteLine($"<< Deleting: {run}");
+            var response = await client.DeleteAsync(deleteUrl(run.id));
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($">> Code {(int)response.StatusCode}: {content}");
+            if (((int)response.StatusCode) / 100 != 2) throw new("Response error");
+        }
     }
+    if (!any) break;
 }
 
 record struct Workflows(int total_count, Workflow[] workflow_runs);
-record struct Workflow(int id, string name, string created_at);
+record struct Workflow(int id, string name, string created_at, string status);
